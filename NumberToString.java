e6d8137a11858395fcadd4258c5cfc2694365dbf
qiu51hua <512300471@qq.com>
2019-04-20 12:16:04 +0800

    创建一个java类文件
    这是个java测试

---
+++ NumberToString.java
@@ -0,0 +1,16 @@
+package com.java1234;
+import java.text.DecimalFormat;
+
+public class NumberToString {
+
+	public static void main(String[] args) {
+		Double d = new Double("1.1235");
+		System.out.println(d);
+		DecimalFormat df = new DecimalFormat("#.###");
+		String strNum = df.format(d);
+		System.out.println(strNum);
+		String result = String.format("%.3f", d);
+		System.out.println(result);
+	}
+
+}
